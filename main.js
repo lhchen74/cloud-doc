28f7271027f53f6f5c35dffa5ab0b80b8ef430a6
lhchen74 <2938449682@qq.com>
2021-01-23 18:15:06 +0800

    Environment Initial

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 680,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    const urlLocation = isDev ? 'http://localhost:3000' : 'dummyUrl'
+    mainWindow.loadURL(urlLocation)
+})
