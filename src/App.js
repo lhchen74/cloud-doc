5ee64d18e58800da52e96e2a194c9830cf23ee3f
lhchen74 <2938449682@qq.com>
2021-01-31 15:27:04 +0800

    FileSearch Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import FileSearch from './components/FileSearch'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      <div className="row">
+        <div className="col left-panel">
+          <FileSearch onFileSearch={(value) => {console.log(value)} }/>
+        </div>
+        <div className="col bg-primary right-panel">
+          <h3>right</h3>
+        </div>
+      </div>
     </div>
   );
 }
